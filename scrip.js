7976b14fd83fc0ee652afd3376fef74e7a737972
jimanue <136768861+jimanue@users.noreply.github.com>
2023-08-24 17:27:39 -0500

    Ejercicios
    `git add .`
    
    `git commit -m "Ejercicios Unidad 3"`
    
    `git push -u`

---
+++ scrip.js
@@ -0,0 +1,17 @@
+constbtn-document.querySelector('button');
+constbody-document.querySelector('body');
+
+constcolores-['green','black','white']
+
+
+btn.addEventListener('click',fondo);
+
+
+
+function fondo(){
+
+    const indiceparseInt-(Math.randon()*colores.length);
+
+    Body.style.backgroundColor-colores[indice];
+
+}
